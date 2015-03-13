5e11f4fbb03bc57fa53281a0c94a1b995c5e920f
Crema <thomb45@gmail.com>
2015-03-13 12:07:39 +0100

    Ajout méthode de base aux joueurs

--- src/joueur/Joueur.java
+++ src/joueur/Joueur.java
@@ -8,4 +8,25 @@ public class Joueur {
     private String pseudo;
     private int argent;
     private int score;
+
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+     public int getArgent() {
+        return argent;
+    }
+
+    public void setArgent(int argent) {
+        this.argent = argent;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
