ca3e178a409c56f9a4f39e8b400ed0366f7fce99
Yannis <yancip@hotmail.fr>
2015-04-01 11:09:08 +0200

    Ajout de la classe de Test pour la classe Plateau

---
+++ src/tests/PlateauTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import gameMaster.MaitreDuJeu;
+import joueur.Joueur;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import plateau.Canal;
+import plateau.Parcelle;
+import plateau.Plateau;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class PlateauTest {
+
+/*
+    public void testTrouveAdjacentVerti(){
+        Plateau p = new Plateau();
+        Canal canal = new Canal(false,4,2,4,4);
+        Parcelle elem = new Parcelle(1,0,false,false, Parcelle.typeChamps.vide,2,3);
+        Parcelle elem = new Parcelle(1,0,false,false, Parcelle.typeChamps.vide,2,4);
+        Parcelle elem = new Parcelle(1,0,false,false, Parcelle.typeChamps.vide,3,3);
+        Parcelle elem = new Parcelle(1,0,false,false, Parcelle.typeChamps.vide,3,4);
+
+
+       tester avec les 4 parcelles :
+        AssertTrue(p.trouveAdjacentVerti(canal,elem));
+    }
+
+
+    //faire la meme avec trouveAdjacentHori(canal,elem)
+        public void testTrouveAdjacentHori(){
+        Plateau p = new Plateau();
+        Canal canal = new Canal(false,2,4,4,4);
+        Parcelle elem = new Parcelle(1,0,false,false, Parcelle.typeChamps.vide,3,2);
+        Parcelle elem = new Parcelle(1,0,false,false, Parcelle.typeChamps.vide,4,2);
+        Parcelle elem = new Parcelle(1,0,false,false, Parcelle.typeChamps.vide,3,3);
+        Parcelle elem = new Parcelle(1,0,false,false, Parcelle.typeChamps.vide,4,3);
+
+        tester avec les 4 parcelles :
+        AssertTrue(p.trouveAdjacentVerti(canal,elem));
+    }
+
+  */
+}
