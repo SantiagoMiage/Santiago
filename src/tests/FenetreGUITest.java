8726eed49b1a3fbcce6dd15dd7ada3d52796869a
Crema <thomb45@gmail.com>
2015-04-19 14:58:04 +0200

    FenetreTest : Ajout test sur montant enchère joueurs

---
+++ src/tests/FenetreGUITest.java
@@ -0,0 +1,71 @@
+package tests;
+
+import gameMaster.MaitreDuJeu;
+import joueur.Joueur;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class FenetreGUITest {
+
+
+    @Test
+    //cas nominal
+    public void testOffreJoueur(){
+        ArrayList<Joueur> listeJoueurs = new ArrayList<Joueur>(4);
+        listeJoueurs.add(new Joueur("Matthieu", 10));
+        listeJoueurs.add(new Joueur("Yannis", 10));
+        listeJoueurs.add(new Joueur("Soraya", 10));
+        listeJoueurs.add(new Joueur("Thomas", 10));
+        MaitreDuJeu mj = new MaitreDuJeu(listeJoueurs);
+        int[] montantEnchere = new int[listeJoueurs.size()];
+
+        boolean res=mj.getFenetre().enchereOk(listeJoueurs.get(0), 5, montantEnchere);
+        assertFalse(res);
+    }
+
+    @Test
+    public void testOffreJoueursNegative(){
+        ArrayList<Joueur> listeJoueurs = new ArrayList<Joueur>(4);
+        listeJoueurs.add(new Joueur("Matthieu", 10));
+        listeJoueurs.add(new Joueur("Yannis", 10));
+        listeJoueurs.add(new Joueur("Soraya", 10));
+        listeJoueurs.add(new Joueur("Thomas", 10));
+        MaitreDuJeu mj = new MaitreDuJeu(listeJoueurs);
+        int[] montantEnchere = new int[listeJoueurs.size()];
+
+        boolean res=mj.getFenetre().enchereOk(listeJoueurs.get(0),-5, montantEnchere);
+        assertTrue(res);
+    }
+
+    @Test
+    public void testOffreJoueursSuperieurMontantJoueur(){
+        ArrayList<Joueur> listeJoueurs = new ArrayList<Joueur>(4);
+        listeJoueurs.add(new Joueur("Matthieu", 10));
+        listeJoueurs.add(new Joueur("Yannis", 10));
+        listeJoueurs.add(new Joueur("Soraya", 10));
+        listeJoueurs.add(new Joueur("Thomas", 10));
+        MaitreDuJeu mj = new MaitreDuJeu(listeJoueurs);
+        int[] montantEnchere = new int[listeJoueurs.size()];
+
+        boolean res=mj.getFenetre().enchereOk(listeJoueurs.get(0),15, montantEnchere);
+        assertTrue(res);
+    }
+
+    @Test
+    public void testOffreJoueursDejaExistante(){
+        ArrayList<Joueur> listeJoueurs = new ArrayList<Joueur>(4);
+        listeJoueurs.add(new Joueur("Matthieu", 10));
+        listeJoueurs.add(new Joueur("Yannis", 10));
+        listeJoueurs.add(new Joueur("Soraya", 10));
+        listeJoueurs.add(new Joueur("Thomas", 10));
+        MaitreDuJeu mj = new MaitreDuJeu(listeJoueurs);
+        int[] montantEnchere = new int[listeJoueurs.size()];
+        montantEnchere[0] = 4;
+        boolean res=mj.getFenetre().enchereOk(listeJoueurs.get(0),4, montantEnchere);
+        assertTrue(res);
+    }
+}
