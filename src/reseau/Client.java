95b91b2a5a70b19d8b76ad290b9acb937c3329bb
Crema <thomb45@gmail.com>
2015-04-25 16:23:32 +0200

    client : commit before hard modif

--- src/reseau/Client.java
+++ src/reseau/Client.java
@@ -1,7 +1,6 @@
 package reseau;
 
 import joueur.Joueur;
-import plateau.Parcelle;
 import plateau.PileParcelle;
 
 import java.io.*;
@@ -11,7 +10,7 @@ import java.util.ArrayList;
 public class Client {
 
     private int codemess;
-    private String mess = "unknow";
+    private String pseudo = "unknow";
     private Boolean stopclient = false;
 
     Socket clientSocket = null;
@@ -23,6 +22,7 @@ public class Client {
 
     ArrayList<Joueur> listeJoueur = null;
     private ArrayList<PileParcelle> pileParcelles = null;
+    private int[] montantEnchere;
 
     public void lancer() {
 
@@ -61,70 +61,87 @@ public class Client {
                     System.out.println("Send");
                     System.out.println(os);
                     System.out.println(clientSocket);
-                    System.err.println( "Something is wrong. One variable is null." );
+                    System.err.println("Something is wrong. One variable is null.");
                     return;
                 }
+                synchronized (this) {
+                    try {
+                        while (true) {
 
-                try {
-                    while ( true ) {
+                            String responseLine = is.readLine();
+                            System.out.println("recu : " + responseLine);
 
-                        String responseLine = is.readLine();
-                        System.out.println("recu : " + responseLine);
+                            if (Integer.parseInt(responseLine) == 1) {
+                                while (pseudo.equals("unknow")) {
 
-                        if(Integer.parseInt(responseLine) == 1) {
-                            while(mess.equals("unknow")){
-
-                            }
-                            os.writeBytes(Integer.toString(1) + "\n");
-                            os.writeBytes(mess+ "\n");
-                            System.out.println("pseudo send");
-                        }
-
-                        if(Integer.parseInt(responseLine) == 2){
-                            System.out.println("récupération de la liste des joueurs");
-                            listeJoueur = (ArrayList<Joueur>) ois.readObject();
-                            System.out.println(listeJoueur);
-                            os.writeBytes(2 + "\n");
-                            synchronized (att) {
-                                att.notify();
+                                }
+                                os.writeBytes(Integer.toString(1) + "\n");
+                                os.writeBytes(pseudo + "\n");
+                                System.out.println("pseudo send");
                             }
 
-                        }
+                            if (Integer.parseInt(responseLine) == 2) {
+                                System.out.println("récupération de la liste des joueurs");
+                                listeJoueur = new ArrayList<Joueur>();
+                                for (int i = 0; i < 4; i++) {
+                                    listeJoueur.add((Joueur) ois.readObject());
+                                }
+                                System.out.println(listeJoueur);
+                                os.writeBytes(2 + "\n");
+                                synchronized (att) {
+                                    att.notify();
+                                }
 
-                        if(Integer.parseInt(responseLine) == 3){
-                            System.out.println("récupération des piles parcelles");
-
-                            pileParcelles = (ArrayList<PileParcelle>) ois.readObject();
-                            System.out.println(pileParcelles);
-                            os.writeBytes(2 + "\n");
-                            synchronized (att) {
-                                att.notify();
                             }
+
+                            if (Integer.parseInt(responseLine) == 3) {
+                                System.out.println("récupération des piles parcelles");
+                                pileParcelles = new ArrayList<PileParcelle>();
+                                for (int i = 0; i < 4; i++) {
+                                    System.out.println("recup parti : " + i);
+                                    pileParcelles.add((PileParcelle) ois.readObject());
+                                }
+                                System.out.println(pileParcelles);
+                                os.writeBytes(2 + "\n");
+                                synchronized (att) {
+                                    att.notify();
+                                }
+                            }
+
+                            if (Integer.parseInt(responseLine) == 4) {
+                                System.out.println("récupération des enchères");
+
+                                montantEnchere = (int[]) ois.readObject();
+                                System.out.println(montantEnchere);
+                                os.writeBytes(2 + "\n");
+                                synchronized (att) {
+                                    att.notify();
+                                }
+                            }
+
+
+                            if (stopclient) {
+                                break;
+                            }
+
+                            //String responseLine = is.readLine();
+                            //System.out.println("Server returns its square as: " + responseLine);
                         }
 
+                        // clean up:
+                        // close the output stream
+                        // close the input stream
+                        // close the socket
 
-
-                        if ( stopclient ) {
-                            break;
-                        }
-
-                        //String responseLine = is.readLine();
-                        //System.out.println("Server returns its square as: " + responseLine);
+                        os.close();
+                        clientSocket.close();
+                    } catch (UnknownHostException e) {
+                        System.err.println("Trying to connect to unknown host: " + e);
+                    } catch (IOException e) {
+                        System.err.println("IOException:  " + e);
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
                     }
-
-                    // clean up:
-                    // close the output stream
-                    // close the input stream
-                    // close the socket
-
-                    os.close();
-                    clientSocket.close();
-                } catch (UnknownHostException e) {
-                    System.err.println("Trying to connect to unknown host: " + e);
-                } catch (IOException e) {
-                    System.err.println("IOException:  " + e);
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
                 }
             }
         };
@@ -135,7 +152,7 @@ public class Client {
     }
 
     public void sendPseudo(String pseudo){
-        mess = pseudo;
+        this.pseudo = pseudo;
     }
 
     public ArrayList<Joueur> getJoueurs() {
@@ -169,5 +186,32 @@ public class Client {
             return pileParcelles;
         }
 
+    }
+
+    public int[] getMontantEnchere() {
+        att = new Thread();
+        att.start();
+        synchronized (att) {
+            while (montantEnchere == null) {
+                System.out.println("att");
+                try {
+                    att.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return montantEnchere;
+        }
+    }
+
+    public void sendEnchere(int i) {
+        try {
+            //code message
+            os.writeBytes(3 + "\n");
+            //montantEnchere
+            os.writeBytes(i + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
